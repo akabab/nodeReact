e193a73bc43698f80a45b9dd0ef84062b9fe5e35
Yoann Cribier <yoann_cribier@hotmail.com>
2018-01-27 19:38:43 +0100

    Gifts controller implementing { get, add, remove, notify } methods + gifts.json (fs storage) added

--- back/controllers/Gifts.js
+++ back/controllers/Gifts.js
@@ -1,16 +1,72 @@
+const fs = require('fs')
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+const nodemailer = require('nodemailer');
+const config = require('../config')
+
+const transporter = nodemailer.createTransport(config.mailerTransporter)
+
+const file = 'gifts.json'
+const getGifts = () => readFileAsync(file, 'utf8').then(JSON.parse)
+
 const Gifts = {
-    create: (req, res, next) => {
 
-    },
-    read: (req, res, next) => {
+  create: (req, res, next) => {
+    const { gift } = req.body
+    if (!gift) throw Error(`missing argument 'gift'`)
 
-    },
-    delete: (req, res, next) => {
+    return getGifts()
+      .then(gifts => {
+        gifts.push(gift)
 
-    },
-    notify: (req, res, next) => {
-        // Send a mail to Santa 
-    }
+        return writeFileAsync(file, JSON.stringify(gifts), 'utf8')
+          .then(() => res.json(gifts))
+
+      }).catch(next)
+  },
+
+  read: (req, res, next) => {
+    return readFileAsync(file, 'utf8')
+      .then(JSON.parse)
+      .then(gifts => res.json(gifts))
+      .catch(next)
+  },
+
+  delete: (req, res, next) => {
+    const { index } = req.body
+    if (!Number.isInteger(index)) throw Error(`invalid/missing argument 'index'`)
+
+    return getGifts()
+      .then(gifts => {
+        gifts.splice(index, 1)
+
+        return writeFileAsync(file, JSON.stringify(gifts), 'utf8')
+          .then(() => res.json(gifts))
+      })
+      .catch(next)
+  },
+
+  notify: (req, res, next) => {
+    return getGifts()
+      .then(gifts => {
+        const list = gifts.map(gift => `<li>${gift}</li>`).join('')
+        const mailOptions = {
+          from: 'yoanncribier@gmail.com',
+          to: 'florian@wildcodeschool.fr',
+          subject: `Cher Père Noël, j'ai été sage`,
+          html: `<h3>Ma liste de cadeaux</h3><ul>${list}</ul>`
+        }
+
+        return transporter.sendMail(mailOptions)
+          .then(() => res.json("ok"))
+      })
+      .catch(err => {
+        console.error(err)
+        res.json("nop")
+      })
+  }
+
 }
 
 module.exports = Gifts;
